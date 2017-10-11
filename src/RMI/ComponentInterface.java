4eff3cc92903ad2c5f76dac4d3f10bcfb55ae376
Astrid <astrid.vanneste1@gmail.com>
2017-10-11 12:21:07 +0200

    Server start

--- src/RMI/ComponentInterface.java
+++ src/RMI/ComponentInterface.java
@@ -2,8 +2,10 @@ package RMI;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.Set;
 
 public interface ComponentInterface extends Remote
 {
 	public GenericComponent getComponent (String key) throws RemoteException;
+	public Set<String> getComponentKeys() throws RemoteException;
 }
